282dc0eb656b525bbe7b5080bb19e794d7c1b6a7
“sm-sawada” <arahunn@gmail.com>
2021-11-13 21:19:16 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FUKURO</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
+    <!--favicon--> 
+    <link rel="shortcut icon" href="images/favicon_moon.jpg">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        
+    </header>
+        <section class="main">
+            <h1>fukuro</h1>
+            <div class="login_button">
+                <a href="#signup">sign up</a>
+                <a href="#login">login</a>
+            </div>
+            <section class="form_view">
+                <div class="user area"  id="signup">
+    
+                    <form class="form">
+                        <div class="form__group">
+                            <input type="text" placeholder="Username" class="form__input" />
+                        </div>
+    
+                        <div class="form__group">
+                            <input type="email" placeholder="Email" class="form__input" />
+                        </div>
+    
+                        <div class="form__group">
+                            <input type="password" placeholder="Password" class="form__input" />
+                        </div>
+    
+                        <button class="btn" type="button">Register</button>
+                    </form>
+                </div>
+                <div class="user area" id ="login">
+                    <form class="form">
+                        <div class="form__group">
+                            <input type="email" placeholder="Email" class="form__input" />
+                        </div>
+                        <div class="form__group">
+                            <input type="password" placeholder="Password" class="form__input" />
+                        </div>
+                        <button class="btn" type="button">Login</button>
+                    </form>
+                </div>
+        </section>
+    </section>
+    <footer>
+        <ul>
+            <li><a href="#">FUKUROについて</a></li>
+            <li><a href="#">利用規約</a></li>
+            <li><a href="#">プライバシーポリシー</a></li>
+            <li><a href="#">ヘルプセンター</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+            <li><a href="#">Cookie</a></li>
+            <li><a href="#">採用情報</a></li>
+            <li><a href="#">プロフィール一覧</a></li>
+            <li><a href="#">ブログ</a></li>
+            <li><a href="#">ステータス</a></li>
+            <li><a href="#">広告</a></li>
+            <li><a href="#">マーケティング</a></li>
+            <li><a href="#">設定</a></li>
+            <li>@ 2021 FUKURO engineer TEAM, Inc.</li>
+        </ul>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+</body>
+</html>
